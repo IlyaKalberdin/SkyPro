0cf8f1cb63b8ea7d48e61063a511a73fbaa7188a
Ilya Kalberdin <ilyakalberdin@gmail.com>
2023-09-16 09:11:18 +0300

    fix title_str and add get_upper_str

--- str_func.py
+++ str_func.py
@@ -1,2 +1,12 @@
-def title_str(string):
+def get_title_str(string):
+    """
+    Функция, которая возвращает строку с первыми зглавными буквами
+    """
     return string.title()
+
+
+def get_upper_str(string):
+    """
+    Функция, которая возвращает строку со всеми заглавными буквами
+    """
+    return string.upper()
